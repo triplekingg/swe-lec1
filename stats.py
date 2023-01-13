bd72587ea081eee6bbfb29ae2d08b03d1ad952e0
Manit Gandhi <65847480+triplekingg@users.noreply.github.com>
2023-01-13 16:29:24 +0700

    Add files via upload

--- stats.py
+++ stats.py
@@ -1,18 +1,37 @@
-import sys
 import statistics
-def stat():
-    filepath = str(sys.argv[1])
-    numbers = []
+import re
+import sys
+
+def is_valid(text: str) -> bool:
+    return not bool(re.search(r'[a-zA-Z]',text))
+
+def findStat(item1):
+   print("mean", statistics.mean(item1), "sid", statistics.stdev(item1), "min", min(item1), "max", max(item1)) 
+
+
+def appendFile(filepath):
+    item1 = []
     with open(filepath, 'r') as f:
-        for line in f:
-            numbers.append(int(line))
-    meann = statistics.mean(numbers)
-    std = statistics.stdev(numbers)
-    minn = min(numbers)
-    maxx = max(numbers)
+        for text in f.readlines():
+            if is_valid(text):
+                item1.append(int(text))
+    return item1
+             
+def stat(filepath):
+    item1 = appendFile(filepath[1])
+    item2 = appendFile(filepath[2])
     print('Statistics Summary')
-    print('mean:', meann)
-    print('std:', std)
-    print('min:', minn)
-    print('max:', maxx)
-stat()
+    print(filepath[1])
+    findStat(item1)
+    print(filepath[2])
+    findStat(item2)
+    findStat(item1+item2)
+    
+
+def main():
+    filepath = sys.argv
+    if len(filepath) < 3:
+        print("need 2 argument")
+    else:
+        stat(filepath)
+main()
